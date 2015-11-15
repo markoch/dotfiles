3bb82d763db3e12cf03308878f83effc07ab0360
Marco Koch <mkoch@marco-koch.net>
2015-11-15 12:20:18 +0100

    Alias enhancements

--- alias.cmd
+++ alias.cmd
@@ -1,12 +1,5 @@
 @echo off
 REM ---------------------------------------------------------------------
-REM Installation instruction
-REM ---------------------------------------------------------------------
-REM HKEY_CURRENT_USER -> Software -> Microsoft -> Command Processor
-REM Add String value with name AutoRun
-REM Value: e.g. %USERPROFILE%\alias.cmd
-
-REM ---------------------------------------------------------------------
 REM Temporary system path at cmd startup
 REM ---------------------------------------------------------------------
 set PATH=%PATH%;"C:\Program Files\Sublime Text 3\"
@@ -14,17 +7,17 @@ set PATH=%PATH%;"C:\Program Files\Sublime Text 3\"
 REM ---------------------------------------------------------------------
 REM Commands
 REM ---------------------------------------------------------------------
-REM Navigation
+REM navigation
 DOSKEY ..=cd ..
 DOSKEY ....=cd ../..
 
-REM Tools
+REM tools
 DOSKEY chrome="C:\Program Files\Google\Chrome\Application\chrome.exe"
 DOSKEY canary="C:\Users\Marco\AppData\Local\Google\Chrome SxS\Application\chrome.exe"
 DOSKEY st=sublime_text $*
 DOSKEY sublime=sublime_text $*
 
-REM command shortcuts
+REM shortcuts
 DOSKEY g=git $*
 DOSKEY ls=dir /B
 
@@ -32,11 +25,14 @@ REM alias file handling
 DOSKEY alias=sublime_text d:\dev\tools\alias.cmd
 DOSKEY alias_copy=copy d:\dev\tools\alias.cmd %USERPROFILE%\Dropbox\dev\env
 
+REM ---------------------------------------------------------------------
 REM Common directories
 REM ---------------------------------------------------------------------
 REM development directories
-DOSKEY d=cd "D:\dev\git\web"
-DOSKEY h=cd "%USERPROFILE%\$*"
+DOSKEY s=cd "D:\dev\git\server"
+DOSKEY c=cd "D:\dev\git\web"
+DOSKEY t=cd "D:\dev\git\web\test"
 
 REM standard directories
+DOSKEY h=cd "%USERPROFILE%\$*"
 DOSKEY db=cd "%USERPROFILE%\Dropbox\$*"
